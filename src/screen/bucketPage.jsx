f108966e2225a254864511b50063d12c95c5647f
Андрей Петров <79992614510@ya.ru>
2023-02-28 12:33:10 +0300

    updated

--- src/screen/bucketPage.jsx
+++ src/screen/bucketPage.jsx
@@ -4,12 +4,8 @@ import "./bucketPage.scss"
 function BucketPage() {
   return (
     <div className="bucketpage">
-      <div className="bucketpage__item">
-        <h5>труба металлопластиковая 26x3.0</h5>
-        <h6>2 х 168.9 руб.</h6>
-      </div>
       <div className="bucketpage__final d-grid gap-2">
-        <h3>Итого: 337.8 руб.</h3>
+        <h3>Итого: 0 руб.</h3>
         <button className="btn btn-primary">Оформить заказ</button>
       </div>
     </div>
